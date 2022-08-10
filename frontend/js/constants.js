9cabc4155a5191ca910ec3efaf3e5256354258ab
Isti10001 <107425156+Isti10001@users.noreply.github.com>
2022-08-10 16:09:37 +0200

    color image

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -1,10 +1,10 @@
 const contractAddress = "0x96f4D02c393596b9c76f0e5993D9a2356C4d0Be7"; // Replace with your own contract address
 const chain = 'rinkeby'; // rinkeby, polygon, or ethereum
 
-const welcome_h1 = "Welcome to the CodeCats NFT Project!!";
+const welcome_h1 = "Welcome to the TIFFANY TATUM NFT (TTN) Project!!";
 const welcome_h2 = "Connect to MetaMask to Get Started";
-const welcome_p = 'The CodeCats NFT Project is a decentralized, open-source project that aims to demonstrate how to develope and launch your own NFT Collection. Follow step by step on the <a href="https://youtube.com/codestackr"  target="_blank" rel="noopener noreferrer">codeSTACKr</a> YouTube channel.';
-const h1_presale_coming_soon = "NFT Drop Coming Soon!!";
+const welcome_p = 'The TTN Project is the 1st NFT collection ever, where a famous erotic modell clother are made cartoon';
+const h1_presale_coming_soon = "NFT Drop Coming in 2nd edition!!";
 const h1_presale_mint = "Pre-Sale Minting Open!!";
 const h1_public_mint = "Public Minting Open!!";
 const h2_presale_coming_soon = "Pre-Sale Minting Countdown";
